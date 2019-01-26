004a7f1b002d877947e0356969386009c2a00c7e
jjurm <micko.juraj64@gmail.com>
2019-01-26 19:45:41 +0000

    Add lunch.py

---
+++ lunch.py
@@ -0,0 +1,24 @@
+import uuid
+
+
+class Restaurant:
+    def __init__(self, name, description, location, rating):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.rating = rating
+
+
+class Lunch:
+    uuid = uuid.uuid4()
+    meal_preferences = []
+    restaurant_type_preferences = []
+
+    restaurants = None  # Fetch restaurants as a list
+
+    def __init__(self, location, date):
+        self.location = location
+
+    def fetch_restaurants(self):
+        pass
+
